dbd8320b2965ef8044fb8ef724ad96fda9d4a43f
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-03-11 16:31:56 -0700

    restructure project

---
+++ src/config.rs
@@ -0,0 +1,6 @@
+pub const TEST_REPO: &str = "https://github.com/AndrewKovalenko/lotogen";
+
+pub const GITHUB_PRIVATE_KEY_FILE: &str = "GITHUB_PRIVATE_KEY_FILE";
+pub const GITHUB_APP_ID: &str = "GITHUB_APP_ID";
+
+pub const GITHUB_JWT_TTL: i64 = 10 * 60; // 10 minutes
