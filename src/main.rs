0264b0c248c051c539b60094ec205739764e44e0
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-03-11 16:31:39 -0700

    shaped github token claims

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use release_notewriter::application::notes::generate_notes_since_latest_release;
-
-const TEST_REPO: &str = "https://github.com/AndrewKovalenko/lotogen";
+use release_notewriter::config::TEST_REPO;
+use release_notewriter::repositories::system;
 
 #[tokio::main]
 async fn main() {
+    system::load_env_file();
     let release_notes = generate_notes_since_latest_release(TEST_REPO).await;
 
     release_notes.iter().for_each(|line| println!("{line}"));
