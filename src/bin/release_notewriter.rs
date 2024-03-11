1ea0e1ce9c1bee483d37a0fe0d5004aa8747ac72
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-03-11 11:43:42 -0700

    try to make a first call to github api

--- src/bin/release_notewriter.rs
+++ src/bin/release_notewriter.rs
@@ -2,8 +2,9 @@ use release_notewriter::application::notes::generate_notes_since_latest_release;
 
 const TEST_REPO: &str = "https://github.com/AndrewKovalenko/lotogen";
 
-fn main() {
-    let release_notes = generate_notes_since_latest_release(TEST_REPO);
+#[tokio::main]
+async fn main() {
+    let release_notes = generate_notes_since_latest_release(TEST_REPO).await;
 
     release_notes.iter().for_each(|line| println!("{line}"));
 }
--- src/data_access/github.rs
+++ src/data_access/github.rs
@@ -1,14 +1,46 @@
 use crate::application::dtos::release::Release;
-use chrono;
+use chrono::{self, DateTime, Utc};
+
+const APP_AUTH_TOKEN: &str = "";
+
+fn parse_url(url: &str) -> (String, String) {
+    (String::from("AndrewKovalenko"), String::from("lotogen"))
+}
 
 pub struct Repository<'a> {
     url: &'a str,
 }
 
 impl<'a> Repository<'a> {
+    pub fn new(url: &'a str) -> Self {
+        Repository { url }
+    }
+
     pub fn get_latest_release(&self) -> Release {
         return Release {
             timestamp: chrono::offset::Utc::now(),
         };
+    }
+
+    pub async fn get_commit(&self, since: DateTime<Utc>, until: DateTime<Utc>) -> String {
+        let (owner, repo) = parse_url(self.url);
+        let get_commits_url = format!("https://api.github.com/repos/{}/{}/commits", owner, repo);
+        let http_client = reqwest::Client::new();
+        let commits = http_client
+            .get(get_commits_url)
+            .header(
+                reqwest::header::AUTHORIZATION,
+                format!("Bearer {APP_AUTH_TOKEN}"),
+            )
+            .header(reqwest::header::ACCEPT, "application/vnd.github+json")
+            .header("X-GitHub-Api-Version", "2022-11-28")
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+
+        return commits;
     }
 }
