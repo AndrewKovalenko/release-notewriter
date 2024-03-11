a47091b309628d8ff3ed7954e8fa791726fd1003
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-03-11 11:43:42 -0700

    try to make a first call to github api

--- src/application/notes.rs
+++ src/application/notes.rs
@@ -1,3 +1,9 @@
-pub fn generate_notes_since_latest_release(repository_url: &str) -> Vec<String> {
-    return vec![];
+use crate::data_access::github::Repository;
+pub async fn generate_notes_since_latest_release(repository_url: &str) -> Vec<String> {
+    let repository = Repository::new(repository_url);
+    // let latest_release = repository.get_latest_release();
+    let commits = repository
+        .get_commit(chrono::offset::Utc::now(), chrono::offset::Utc::now())
+        .await;
+    return vec![commits];
 }
