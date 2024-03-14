fee1b642513c98281207ade528005d14bd4b9ee6
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-03-14 13:50:51 -0700

    add ability to generate jwt

--- src/services/jwt.rs
+++ src/services/jwt.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
+use std::fs;
 
+use anyhow::bail;
 use chrono::Duration;
-use jsonwebtoken;
+use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
 use serde::Serialize;
 
 const ONE_MINUTE: i64 = 60;
@@ -12,13 +13,16 @@ struct GitHubClaims {
     exp: i64,
     iss: String,
 }
+fn read_private_key(file_path: &str) -> anyhow::Result<String> {
+    fs::read_to_string(file_path).or(bail!("unable to read secret form file: {file_path}"))
+}
 
 pub fn generate_github_access_jwt(
     app_id: &str,
     private_key_file_path: &str,
     ttl_sec: i64,
 ) -> String {
-    let jwt_headers = HashMap::from([("typ", "JWT"), ("alg", "RS256")]);
+    let jwt_header = Header::new(Algorithm::RS256);
 
     let now = chrono::offset::Local::now();
     let one_minute_ago = now - Duration::seconds(ONE_MINUTE);
@@ -29,4 +33,27 @@ pub fn generate_github_access_jwt(
         exp: jwt_expiration_time.timestamp(),
         iss: String::from(app_id),
     };
+
+    let private_key = read_private_key(private_key_file_path).unwrap();
+
+    let jwt_token = encode(
+        &jwt_header,
+        &claims,
+        &EncodingKey::from_secret(private_key.as_ref()),
+    );
+
+    jwt_token.unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use jsonwebtoken::{Algorithm, Header};
+
+    #[test]
+    fn sandbox() {
+        let jwt_header = Header::new(Algorithm::RS256);
+
+        println!("Type: {}", jwt_header.typ.unwrap());
+        println!("Claims: {}", cla);
+    }
 }
