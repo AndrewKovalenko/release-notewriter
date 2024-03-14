51ea5d1441024927a1a0f0fe7119c8c63f38e4b8
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-03-14 15:23:39 -0700

    properly read commits since latest release

--- src/repositories/github.rs
+++ src/repositories/github.rs
@@ -1,9 +1,14 @@
-use crate::{application::dtos::release::Release, config, services::jwt};
+use crate::{
+    application::dtos::release::{GitHubTimeStamp, Release},
+    config,
+    services::jwt,
+};
 use chrono::{self, DateTime, Utc};
 
 use super::system;
 
 const APP_AUTH_TOKEN: &str = "ghs_2bfqNh2rReCtw5MVfXffTREXTkc7Ka2u8Odn";
+
 fn parse_url(url: &str) -> (String, String) {
     (String::from("AndrewKovalenko"), String::from("lotogen"))
 }
@@ -17,22 +22,17 @@ impl<'a> Repository<'a> {
         Repository { url }
     }
 
-    pub fn get_latest_release(&self) -> Release {
-        return Release {
-            timestamp: chrono::offset::Utc::now(),
-        };
-    }
-
-    pub async fn get_commit(&self, since: DateTime<Utc>, until: DateTime<Utc>) -> String {
-        let app_id = system::get_env_value(config::GITHUB_APP_ID).unwrap();
-        let private_key_pass = system::get_env_value(config::GITHUB_PRIVATE_KEY_FILE).unwrap();
-
-        let jwt_token =
-            jwt::generate_github_access_jwt(app_id.as_str(), private_key_pass.as_str(), 600);
-        println!("{}", jwt_token);
-
+    pub async fn get_commits(&self, since: Option<GitHubTimeStamp>) -> String {
         let (owner, repo) = parse_url(self.url);
-        let get_commits_url = format!("https://api.github.com/repos/{}/{}/commits", owner, repo);
+        let get_commits_url = if let Some(timestamp) = since {
+            format!(
+                "https://api.github.com/repos/{}/{}/commits?since={}",
+                owner, repo, timestamp
+            )
+        } else {
+            format!("https://api.github.com/repos/{}/{}/commits", owner, repo)
+        };
+
         let http_client = reqwest::Client::new();
         let commits = http_client
             .get(get_commits_url)
@@ -48,5 +48,33 @@ impl<'a> Repository<'a> {
             .unwrap();
 
         return commits;
+    }
+
+    pub async fn get_latest_release(&self) -> Option<Release> {
+        let (owner, repo) = parse_url(self.url);
+        let latest_release_api_call =
+            format!(" https://api.github.com/repos/{owner}/{repo}/releases/latest");
+
+        let http_client = reqwest::Client::new();
+        let latest_release_response = http_client
+            .get(latest_release_api_call)
+            .header("Authorization", APP_AUTH_TOKEN)
+            .header(reqwest::header::USER_AGENT, "Release-Noter")
+            .header(reqwest::header::ACCEPT, "application/vnd.github+json")
+            .header("X-GitHub-Api-Version", "2022-11-28")
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+
+        if let Ok(latest_release) =
+            serde_json::from_str::<Release>(latest_release_response.as_str())
+        {
+            return Some(latest_release);
+        }
+
+        None
     }
 }
