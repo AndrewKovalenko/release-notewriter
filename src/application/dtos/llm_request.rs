9d4a39c304bca8d6605aea87bc7100650f352f3a
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-05-11 18:16:33 -0700

    add DTOs for communication with chat GPT

---
+++ src/application/dtos/llm_request.rs
@@ -0,0 +1,42 @@
+const GPT_3_5_TURBO_NAME: &str = "gpt-3.5-turbo";
+const SYSTEM_ROLE_NAME: &str = "system";
+const ASSISTANT_ROLE_NAME: &str = "assistant";
+const USER_ROLE_NAME: &str = "user";
+
+pub enum ModelVersion {
+    Gpt3_5_Turbo,
+}
+
+impl fmt::Display for ModelVersion {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ModelVersion::Gpt3_5_Turbo => write!(f, GPT_3_5_TURBO_NAME),
+        }
+    }
+}
+
+pub enum GptRole {
+    System,
+    Assistant,
+    User,
+}
+
+impl fmt::Display for GptRole {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            GptRole::System => write!(f, SYSTEM_ROLE_NAME),
+            GptRole::Assistant => write!(f, ASSISTANT_ROLE_NAME),
+            GptRole::User => write!(f, USER_ROLE_NAME),
+        }
+    }
+}
+
+pub struct GptMessage {
+    pub role: GptRole,
+    pub content: String,
+}
+
+pub struct LLM_Request {
+    pub model: ModelVersion,
+    pub message: Vec<GptMessage>,
+}
